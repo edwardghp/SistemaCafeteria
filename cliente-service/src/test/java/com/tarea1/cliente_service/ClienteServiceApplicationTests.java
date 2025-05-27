91bce2fbee82e1ca008664fc1ccd26ed5e6daa48
edwardghp <edward.ortega340@gmail.com>
2025-05-27 01:31:02 -0500

    Microservicio Cliente #2

---
+++ cliente-service/src/test/java/com/tarea1/cliente_service/ClienteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea1.cliente_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
